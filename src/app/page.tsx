321e1ab0ce3006d36fa6d1ec2f395a6cf8bf878a
anishshobithps <anish.shobith19@gmail.com>
2024-03-27 18:22:54 +0530

    fix: log vercel URL

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,6 +14,7 @@ export default async function Home() {
       src: `${baseURL}${imageName}`,
     };
   });
+  console.log(baseURL, images)
   return (
     <main className="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 p-4 md:gap-8 md:p-10">
       <Starfield
