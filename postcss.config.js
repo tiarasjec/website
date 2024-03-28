71fd90dee2da3e824215f48bac14f42b5573cd66
anishshobithps <anish.shobith19@gmail.com>
2024-03-28 08:33:26 +0530

    fix: postconfig errors

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,6 @@
 module.exports = {
-  plugins: [
-    require('postcss-nesting'),
-    require('tailwindcss'),
-    require('autoprefixer'),
-  ],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
