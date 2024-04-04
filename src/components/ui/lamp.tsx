2ce221f12be78de585e9e4774ec62b36e8c61f69
Mohammed Rabil <rabil@techie.com>
2024-04-04 16:42:46 +0530

    fix: Refactor lamp container styling

--- src/components/ui/lamp.tsx
+++ src/components/ui/lamp.tsx
@@ -13,7 +13,7 @@ export const LampContainer = () => {
   });
 
   return (
-    <div className="lampcontainer z-40 flex rotate-270 opacity-65 pt-20 -my-20">
+    <div className="lampcontainer z-40 flex rotate-270 opacity-65 pt-20">
       <div
         className={cn(
           { "scale-[3] md:scale-[2] 2xl:scale-[1.55]": inView },
