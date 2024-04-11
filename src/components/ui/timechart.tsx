29f52dfdef25c88ca6f249a0b9011a0470854ae1
melroydsa <dsamelroy5@gmail.com>
2024-04-11 12:39:37 +0530

    timechart

--- src/components/ui/timechart.tsx
+++ src/components/ui/timechart.tsx
@@ -1,46 +1,32 @@
 import React from "react";
 
 const timechart = () => {
+  const events = [
+    "Registration",
+    "Inaugration",
+    "Capture Clash",
+    "Reelingreels",
+    "Headline Hust..",
+  ];
   return (
-    <div className=" w-full h-fit items-center space-y-3 bg-blue-500">
-      <div className="relative w-full h-fit flex items-center font-staat">
-        <div className="absolute h-0.5 w-full bg-primary rounded-full" />
-        <div className="relative h-12 w-1/6 ml-2 bg-secondary rounded-full flex items-center">
-          <div className="relative z-20 h-12 w-12 bg-tiara_red rounded-full"></div>
-          <h3 className=" ml-2 text-2xl">Registration</h3>
+    <div className="w-full h-fit items-center space-y-3 bg-blue-500">
+      {events.map((event, index) => (
+        <div
+          key={index}
+          className="relative w-full h-fit flex items-center font-staat"
+        >
+          <div className="absolute h-0.5 w-full bg-primary rounded-full" />
+          <div
+            className={`relative h-12 w-1/6 ml-${
+              index * 28 + 2
+            } bg-secondary rounded-full flex items-center`}
+          >
+            <div className="relative z-20 h-12 w-12 bg-tiara_red rounded-full"></div>
+            <h3 className="ml-2 text-2xl">{event}</h3>
+          </div>
         </div>
-      </div>
-      <div className="relative w-full h-fit flex items-center font-staat">
-        <div className="absolute h-0.5 w-full bg-primary rounded-full" />
-        <div className="relative h-12 w-1/6 ml-52 bg-secondary rounded-full flex items-center">
-          <div className="relative z-20 h-12 w-12 bg-tiara_red rounded-full"></div>
-          <h3 className="ml-2 text-2xl">Inaugration</h3>
-        </div>
-      </div>
-      <div className="relative w-full h-fit flex items-center font-staat">
-        <div className="absolute h-0.5 w-full bg-primary rounded-full" />
-        <div className="relative h-12 w-1/6 ml-80 bg-secondary rounded-full flex items-center">
-          <div className="relative z-20 h-12 w-12 bg-tiara_red rounded-full"></div>
-          <h3 className="ml-2 text-2xl">Capture Clash</h3>
-        </div>
-      </div>
-      <div className="relative w-full h-fit flex items-center font-staat">
-        <div className="absolute h-0.5 w-full bg-primary rounded-full" />
-        <div className="relative h-12 w-1/6 ml-96 bg-secondary rounded-full flex items-center">
-          <div className="relative z-20 h-12 w-12 bg-tiara_red rounded-full"></div>
-          <h3 className="ml-2 text-2xl">Reelingreels</h3>
-        </div>
-      </div>
-      <div className="relative w-full h-fit flex items-center font-staat">
-        <div className="absolute h-0.5 w-full bg-primary rounded-full" />
-        <div className="relative h-12 w-1/6 ml-96 bg-secondary rounded-full flex items-center">
-          <div className="relative z-20 h-12 w-12 bg-tiara_red rounded-full"></div>
-          <h3 className="ml-2 text-2xl">Headline Hust..</h3>
-        </div>
-      </div>
+      ))}
     </div>
-
-    
   );
 };
 
