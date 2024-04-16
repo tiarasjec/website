0dc7e8bff9381d6373f5ac27e119144c18ecc7d4
Joywin Bennis <107112207+joywin2003@users.noreply.github.com>
2024-04-16 12:54:46 +0530

    merged

--- src/components/razorpay/BuyProduct.tsx
+++ src/components/razorpay/BuyProduct.tsx
@@ -3,13 +3,47 @@ import React, { Suspense } from "react";
 import Buy from "./Buy";
 import { useRouter } from "next/navigation";
 
+declare global {
+  interface Window {
+    Razorpay: any;
+  }
+}
+
 const BuyProduct = () => {
   const router = useRouter();
+
+  const makePayment = async ({ productId = null }) => {
+    const key = process.env.RAZORPAY_API_KEY;
+    console.log(key);
+    const data = await fetch("http://localhost:3000/api/razorpay");
+    const { order } = await data.json();
+    console.log(order.id);
+    const options = {
+      key: key,
+      name: "Joywin Bennis",
+      currency: order.currency,
+      amount: order.amount,
+      order_id: order.id,
+      description: "Tiara Registration Fee",
+      prefill: {
+        name: "Lord Joywin",
+        email: "joywinbennis@gmail.com",
+        contact: "9354536067",
+      },
+    };
+
+    const paymentObject = new window.Razorpay(options);
+    paymentObject.open();
+
+    paymentObject.on("payment.failed", function (response: any) {
+      alert("Payment failed. Please try again. Contact support for help");
+    });
+  };
 
   return (
     <>
       <Suspense fallback={null}>
-        <Buy />
+        <Buy makePayment={makePayment} />
       </Suspense>
     </>
   );
