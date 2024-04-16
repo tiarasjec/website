1e4282cd0e7e7b5cea805917fce47bf2c19fbcdf
Mohammed Rabil <rabil@techie.com>
2024-04-16 15:06:50 +0530

    fix: Refactor Razorpay integration in route.ts

--- src/app/api/razorpay/route.ts
+++ src/app/api/razorpay/route.ts
@@ -7,30 +7,6 @@ const instance = new Razorpay({
     key_secret: process.env.RAZORPAY_SECRET as string,
   });
 
-
-export async function GET() {
-
-    const payment_capture = 1;
-    const amount = 1 * 100 // amount in paisa. In our case it's INR 1
-    const currency = "INR";
-    const options = {
-        amount: (amount).toString(),
-        currency,
-        receipt: shortid.generate(),
-        payment_capture,
-        notes: {
-            // These notes will be added to your transaction. So you can search it within their dashboard.
-            // Also, it's included in webhooks as well. So you can automate it.
-            paymentFor: "testingDemo",
-            userId: "100",
-            productId: 'P100'
-        }
-    };
-
-   const order = await instance.orders.create(options);
-  return NextResponse.json({ msg: "success",order });
-}
-
 export async function POST(req: Request) {
   const body = await req.json();
   const payment_capture = 1;
