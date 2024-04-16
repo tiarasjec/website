d9f4dabe8498e430f617268f849ce29fb191b1b9
melroydsa <dsamelroy5@gmail.com>
2024-04-16 10:44:49 +0530

    feat: Card component Events

--- src/app/(content)/events/page.tsx
+++ src/app/(content)/events/page.tsx
@@ -7,7 +7,8 @@ import React from "react";
 export default function EventsPage() {
   return (
     <>
-      <div className="absolute w-11/12 flex justify-center left-1/2 transform -translate-x-1/2">
+    
+      <div className="dark absolute w-11/12 flex justify-center left-1/2 transform -translate-x-1/2">
         <div className="flex justify-center items-center mt-10 w-full ">
           <Timeline />
         </div>
--- src/components/widgets/Timeline.tsx
+++ src/components/widgets/Timeline.tsx
@@ -1,25 +1,26 @@
 import React from "react";
 import Timechart from "@/components/ui/timechart";
 
+
 const Timeline = () => {
   return (
-    <div className="w-full h-fit">
-      <div className="flex justify-center">
+    <div className="w-full h-fit flex flex-col items-center">
+      <div className="flex justify-center items-center">
         <div className="font-tiara text-7xl">
           <span className="text-tiara_red">T</span>iara{" "}
           <span className="text-tiara_red">{"'"}</span>24{" "}
         </div>
-        <div className="ml-10 rounded-full w-fit p-4 h-fit bg-gray-400 space-x-10">
-          <button className="rounded-full w-56 h-full pl-3 pr-3 bg-white text-black font-staat text-3xl">
-            9th May
-          </button>
-          <button className="rounded-full w-56 h-full pl-3 pr-3 bg-white text-black font-staat text-3xl">
-            10th May
-          </button>
-          <button className="rounded-full w-56 h-full pl-3 pr-3 bg-white text-black font-staat text-3xl">
-            11th May
-          </button>
-        </div>
+      </div>
+      <div className="mt-5 rounded-full w-fit p-4 h-fit bg-gray-400 space-x-10 flex justify-center">
+        <button className="rounded-full w-56 h-full pl-3 pr-3 bg-white text-black font-staat text-3xl">
+          9th May
+        </button>
+        <button className="rounded-full w-56 h-full pl-3 pr-3 bg-white text-black font-staat text-3xl">
+          10th May
+        </button>
+        <button className="rounded-full w-56 h-full pl-3 pr-3 bg-white text-black font-staat text-3xl">
+          11th May
+        </button>
       </div>
       <div className="mt-3 h-fit w-full flex pt-3">
         <Timechart/>
