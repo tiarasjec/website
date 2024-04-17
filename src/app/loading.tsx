f5ac9029dff0b1b887d5fff352f08984f8abaa77
22c60.vyshnav <22c60.vyshnav@sjec.ac.in>
2024-04-17 05:41:05 +0000

    Add red color for AR instead of T

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,7 +1,7 @@
 export default function Loading() {
   return (
     <div className="font-tiara text-6xl md:text-9xl min-h-screen flex items-center justify-center animate-pulse ">
-      <span className="text-tiara_red">T</span>iara{" "}
+      <span className="">Ti</span><span className="text-tiara_red">ar</span>a{" "}
       <span className="text-tiara_red">{"'"}</span>24{" "}
     </div>
   );
