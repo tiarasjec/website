ede265a1c4e71e8f4133913b9504342422c905a7
22c60.vyshnav <22c60.vyshnav@sjec.ac.in>
2024-04-17 04:51:59 +0000

    feat(loading page)

---
+++ src/app/loading.tsx
@@ -0,0 +1,8 @@
+export default function Loading() {
+  return (
+    <div className="font-tiara text-6xl md:text-9xl min-h-screen flex items-center justify-center animate-pulse ">
+      <span className="text-tiara_red">T</span>iara{" "}
+      <span className="text-tiara_red">{"'"}</span>24{" "}
+    </div>
+  );
+}
