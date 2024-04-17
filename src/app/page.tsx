98f0311587f330b0057e942cf5edf9bb3a682ec9
vyshnav <vyshnavravi599@gmail.com>
2024-04-17 17:52:20 +0530

    Refactor Home component to add loading page till it fetches the image form api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,18 +3,28 @@ import Lenis from "@/components/shared/lenis";
 import { HeroParallax } from "@/components/widgets/Hero";
 import { StarsCanvas } from "@/components/ui/Stars";
 import { useEffect, useState } from "react";
+import Loading from "./loading";
 
 export default function Home() {
   const [images, setImages] = useState<{ src: string; alt: string }[]>([]);
+  const [loading,setLoading ] = useState<boolean> (true)
 
   useEffect(() => {
+    setLoading(true)
     fetch("/api/gallery")
       .then((response) => response.json())
-      .then((data) => setImages(data))
+      .then((data) =>{
+        //setloading to false
+        setLoading(false),
+        setImages(data)
+  })
+  
+      
       .catch((error) => console.error("Error fetching images:", error));
   }, []);
 
   return (
+    loading ? <Loading/> :
     <main className="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 py-0 md:gap-8 md:py-0">
       <Lenis>
         <StarsCanvas />
