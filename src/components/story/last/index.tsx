e76bfc4ed1aefab0f67a504ea0bc7b6f3c36fa73
anishshobithps <anish.shobith19@gmail.com>
2024-04-18 08:58:45 +0530

    feat: some improvements in landing page

---
+++ src/components/story/last/index.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import * as Scrollytelling from "@bsmnt/scrollytelling";
+import s from "./last.module.scss";
+
+export const Last = () => {
+  return (
+    <Scrollytelling.Root start="top bottom" scrub={0.75}>
+      <section className={s["section"]}>
+        <div className="wrapper">
+          <Scrollytelling.Waypoint
+            at={50}
+            tween={{
+              target: ["body"],
+              to: { color: "black" },
+              duration: 0.35,
+            }}
+          />
+          <Scrollytelling.Waypoint
+            at={75}
+            tween={{
+              target: ["#smile-image", "#pc-image"],
+              fromTo: [
+                { opacity: 0, scale: 0.4 },
+                { opacity: 1, scale: 1, ease: "elastic.out(1,0.5)" },
+              ],
+              duration: 1.2,
+            }}
+          />
+          <Scrollytelling.Animation
+            tween={{
+              start: 0,
+              end: 100,
+              fromTo: [
+                {
+                  scale: 0,
+                },
+                {
+                  ease: "linear",
+                  scale: 1,
+                },
+              ],
+            }}
+          >
+            <h2 className={s["title"]}>
+              REGISTER NOW!
+            </h2>
+          </Scrollytelling.Animation>
+        </div>
+      </section>
+    </Scrollytelling.Root>
+  );
+};
