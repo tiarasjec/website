6a0538381f29852f7a2ad34a8e6ab2e35d0b01ab
Joywin Bennis <107112207+joywin2003@users.noreply.github.com>
2024-04-19 00:04:05 +0530

    fixed the pathname bug since someone idiot gave a hardcoded index

--- src/app/api/events/route.ts
+++ src/app/api/events/route.ts
@@ -1,6 +1,9 @@
 import {categoriesList} from "@/data/categoryList";
+import {NextRequest} from "next/server";
 
-export async function GET() {
+export async function GET(req: NextRequest) {
+  const path = req.nextUrl.pathname.split("/");
+  const category = path[3];
   
 
   const mappedCategories = categoriesList.map(({category,events}) => {
