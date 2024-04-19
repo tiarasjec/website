57019ec6ee158fa9a511ac20c14ca1ef72271184
Joywin Bennis <107112207+joywin2003@users.noreply.github.com>
2024-04-19 15:25:52 +0530

    changed the styling

--- src/app/(content)/events/[category]/page.tsx
+++ src/app/(content)/events/[category]/page.tsx
@@ -39,10 +39,10 @@ export default function EventsPage() {
               .sort((a, b) => a.name.localeCompare(b.name))
               .map((card, index) => {
                 return (
-                  <CardContainer className="inter-var border-2 border-tiara_red rounded-lg h-48 w-96 m-20">
+                  <CardContainer className="inter-var border-2 border-tiara_red rounded-lg h-60 w-96 m-20">
                     <CardBody
                       key={card.id}
-                      className="bg-black relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  "
+                      className="bg-gray-900 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 mb-20 border  "
                     >
                       <CardItem
                         translateZ="50"
@@ -50,18 +50,18 @@ export default function EventsPage() {
                       >
                         {card.name}
                       </CardItem>
-                      <CardItem translateZ="50"  as={Link}
+                      <CardItem translateZ="100"  as={Link}
                           href={card.href} className="w-full mt-4">
                         <Image
                           src={card.thumbnail}
-                          height="1000"
-                          width="1000"
-                          className="h-full w-full object-cover rounded-xl group-hover/card:shadow-xl"
+                          height="800"
+                          width="800"
+                          className="w-full object-cover rounded-xl group-hover/card:shadow-xl"
                           alt="thumbnail"
                         />
                       </CardItem>
-                      <div className="flex justify-between items-center mt-20">
-                        {/* <CardItem
+                      {/* <div className="flex justify-between items-center mt-20">
+                        <CardItem
                           translateZ={20}
                           as={Link}
                           href={card.href}
@@ -69,8 +69,8 @@ export default function EventsPage() {
                           className="px-4 py-2 rounded-xl text-sm font-normal text-white"
                         >
                           Check out description
-                        </CardItem> */}
-                      </div>
+                        </CardItem>
+                      </div> */}
                     </CardBody>
                   </CardContainer>
                 );
