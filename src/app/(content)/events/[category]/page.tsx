ca8eb7448ab6962d57a972c754fe566b7c6f5125
Joywin Bennis <107112207+joywin2003@users.noreply.github.com>
2024-04-19 21:03:57 +0530

    fixed missing key props error

--- src/app/(content)/events/[category]/page.tsx
+++ src/app/(content)/events/[category]/page.tsx
@@ -41,7 +41,7 @@ export default function EventsPage() {
                 return (
                   <CardContainer className="inter-var  h-60 w-96 m-20">
                     <CardBody
-                      key={card.id}
+                      key={index}
                       className="bg-gray-900 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 mb-20 border  "
                     >
                       <CardItem
