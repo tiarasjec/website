0c73a5c453c9e2fce26068deaa5e92bcd32b9d18
vyshnav <vyshnavravi599@gmail.com>
2024-04-21 20:40:43 +0530

    Add refund page

---
+++ src/app/(content)/refund/page.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import * as React from "react";
+import Container from "@/components/shared/container";
+import { tiaraFont } from "@/lib/fonts";
+import { cn } from "@/lib/utils";
+import {legalInfo} from "../../../data/legal_Info"
+export default function Refund() {
+   
+    return (
+        <Container className="mx-auto">
+            <div className="py-4 mx-6  sm:mx-12 space-y-4 pt-32">
+                <h1 className={cn("text-5xl", tiaraFont)}>Refund Policy</h1>
+                {legalInfo.Refund.map((refund, index) => (
+                    <div key={index}>
+                        <h1 className={cn("text-2xl", tiaraFont)}>{refund.title}</h1>
+                        <p>{refund.description}</p>
+                    </div>
+               ))}
+            </div>
+        </Container>
+    );
+}
