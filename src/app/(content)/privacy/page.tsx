6fe6dd9e4fe7b025e82eb4d2e76e076d51891f75
vyshnav <vyshnavravi599@gmail.com>
2024-04-22 23:35:48 +0530

    Refactor privacy, refund, and terms pages to use Text component for headings

--- src/app/(content)/privacy/page.tsx
+++ src/app/(content)/privacy/page.tsx
@@ -4,13 +4,14 @@ import * as React from "react";
 import Container from "@/components/shared/container";
 import { tiaraFont } from "@/lib/fonts";
 import { cn } from "@/lib/utils";
-import {legalInfo} from "../../../data/legal_Info"
+import { legalInfo } from "../../../data/legal_Info";
+import { Text } from "@/components/shared/text";
+
 export default function Privacy() {
-   
     return (
         <Container className="mx-auto ">
             <div className="py-4 mx-6  sm:mx-12 space-y-4 pt-32 text-white">
-                <h1 className={cn("text-5xl", tiaraFont)}>Privacy Policy</h1>
+                <Text variant="h1" className={cn("text-5xl", tiaraFont)}>Privacy Policy</Text>
                 <p>
                     At Tiara, we take your privacy seriously and are committed to protecting your personal information. This
                     privacy policy explains how we collect, use, and safeguard your data. By registering for our festival
@@ -18,10 +19,10 @@ export default function Privacy() {
                 </p>
                 {legalInfo.policy.map((policy, index) => (
                     <div key={index}>
-                        <h1 className={cn("text-2xl", tiaraFont)}>{policy.title}</h1>
+                        <Text variant="h2" className={cn("text-2xl", tiaraFont)}>{policy.title}</Text>
                         <p dangerouslySetInnerHTML={{ __html: policy.description }}></p>
                     </div>
-               ))}
+                ))}
             </div>
         </Container>
     );
--- src/app/(content)/refund/page.tsx
+++ src/app/(content)/refund/page.tsx
@@ -4,16 +4,17 @@ import * as React from "react";
 import Container from "@/components/shared/container";
 import { tiaraFont } from "@/lib/fonts";
 import { cn } from "@/lib/utils";
-import {legalInfo} from "../../../data/legal_Info"
+import { legalInfo } from "../../../data/legal_Info"
+import { Text } from "@/components/shared/text";
 export default function Refund() {
    
     return (
         <Container className="mx-auto">
             <div className="py-4 mx-6  sm:mx-12 space-y-4 pt-32 text-white">
-                <h1 className={cn("text-5xl", tiaraFont)}>Refund Policy</h1>
+            <Text variant="h1" className={cn("text-5xl", tiaraFont)}>Refund Policy</Text>
                 {legalInfo.Refund.map((refund, index) => (
                     <div key={index}>
-                        <h1 className={cn("text-2xl", tiaraFont)}>{refund.title}</h1>
+                        <Text variant="h2" className={cn("text-2xl", tiaraFont)}>{refund.title}</Text>
                         <p dangerouslySetInnerHTML={{ __html: refund.description }}></p>
                     </div>
                ))}
