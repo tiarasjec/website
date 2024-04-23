cbd6b26007d55c451f7fe9fe57b2a17214ae2a37
Joywin Bennis <107112207+joywin2003@users.noreply.github.com>
2024-04-23 14:53:08 +0530

    validation done for make payments

--- src/components/razorpay/Buy.tsx
+++ src/components/razorpay/Buy.tsx
@@ -2,7 +2,7 @@
 import { makePayment } from "@/lib/utils";
 import React, { useState } from "react";
 import { Button } from "@/components/ui/button";
-import {Teams} from "@/lib/interfaces";
+import { Teams } from "@/lib/interfaces";
 
 const Buy = (userData: {
   events: string[];
@@ -37,12 +37,15 @@ const Buy = (userData: {
       disabled={
         userData.amount === 0 ||
         userData.college.length === 0 ||
-        userData.contact.length === 10 ||
+        userData.contact.length < 10 ||
         userData.events.length === 0 ||
+        (userData.teams.length > 0 &&
+          userData.teams.some((team) => team.name.length === 0)) ||
         isLoading
           ? true
           : false
       }
+      
       className={` font-semibold py-2 px-4 rounded ${
         isLoading ? "opacity-50 cursor-not-allowed" : ""
       }`}
