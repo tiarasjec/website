6a91374b10f1f712e62baa5fbfde969ed82dbb65
Mohammed Rabil <rabil@techie.com>
2024-04-24 21:56:08 +0530

    feat: Add web team members to CoreTeamPage component

--- src/app/(content)/team/page.tsx
+++ src/app/(content)/team/page.tsx
@@ -25,6 +25,19 @@ const coreTeam = [
   `${tiaraAssetsPrefix}/coreteam/website.avif`,
 ];
 
+const webTeam = [
+  `${tiaraAssetsPrefix}/webteam/anish.avif`,
+  `${tiaraAssetsPrefix}/webteam/rabil.avif`,
+  `${tiaraAssetsPrefix}/webteam/jenison.avif`,
+  `${tiaraAssetsPrefix}/webteam/joywin.avif`,
+  `${tiaraAssetsPrefix}/webteam/vyshnav.avif`,
+  `${tiaraAssetsPrefix}/webteam/vineeth_.avif`,
+  `${tiaraAssetsPrefix}/webteam/melroy.avif`,
+  `${tiaraAssetsPrefix}/webteam/nithin.avif`,
+  `${tiaraAssetsPrefix}/webteam/uthpal.avif`,
+  `${tiaraAssetsPrefix}/webteam/vaibhav.avif`,
+];
+
 export default function CoreTeamPage() {
   return (
     <div className="h-fit">
@@ -43,6 +56,44 @@ export default function CoreTeamPage() {
       <div className="w-full flex justify-center ">
         <div className="p-14 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-16">
           {coreTeam.map((card, index) => {
+            return (
+              <CardContainer
+                key={index}
+                containerClassName="relative flex items-center justify-center transition-all duration-200 ease-linear"
+              >
+                <CardBody className="relative">
+                  <CardItem translateZ="100" className="w-full mt-4">
+                    <Image
+                      src={card}
+                      className="rounded-xl "
+                      alt="thumbnail"
+                      width={1200}
+                      height={800}
+                      priority
+                      sizes="(max-width: 640px) 100vw, (max-width: 1023px) 50vw, 33vw"
+                    />
+                  </CardItem>
+                </CardBody>
+              </CardContainer>
+            );
+          })}
+        </div>
+      </div>
+      <div className="-ml-5 flex justify-center items-center -mt-16 z-50">
+        <div
+          className={cn(
+            "text-4xl xs:text-5xl sm:text-6xl md:text-7xl lg:text-8xl w-fit text-center duration-500",
+            tiaraFont.className
+          )}
+        >
+          <h1 className="text-4xl xs:text-5xl sm:text-6xl md:text-7xl lg:text-8xl">
+            Website Team
+          </h1>
+        </div>
+      </div>
+      <div className="w-full flex justify-center ">
+        <div className="p-14 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-16">
+          {webTeam.map((card, index) => {
             return (
               <CardContainer
                 key={index}
