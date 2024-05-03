a16245fadaa9b728402775f3fb4ba55b525f9001
anishshobithps <anish.shobith19@gmail.com>
2024-05-03 22:32:10 +0530

    rm: actions

--- src/actions/user.ts
+++
@@ -1,9 +0,0 @@
-import { prisma } from "@/lib/prisma";
-
-export async function getUserByEmail(email: string) {
-  return await prisma.user.findUnique({
-    where: {
-      email: email,
-    },
-  });
-}
