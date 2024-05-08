470ee7e4498a91e9554268ea7f0364e0a0dc9449
anishshobithps <anish.shobith19@gmail.com>
2024-05-08 14:00:38 +0530

    fix: redis, connection issues and handle the error

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -2,6 +2,8 @@ import IORedis, { RedisOptions } from 'ioredis';
 
 const options: RedisOptions = {
     maxRetriesPerRequest: null,
+    connectTimeout: 10000,
+    keepAlive: 1000,
     tls: {
         rejectUnauthorized: false,
     }
@@ -11,4 +13,8 @@ export const connection = new IORedis(process.env.REDIS_KV!, options);
 
 connection.once("connect", () => {
     console.log('Connected to Redis');
+});
+
+connection.on("error", (error) => {
+    console.error('Redis error:', error);
 });
